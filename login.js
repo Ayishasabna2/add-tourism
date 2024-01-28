f5df13ccab555237fe294c3e407273fe83b94d61
Ayishasabna2 <152488533+Ayishasabna2@users.noreply.github.com>
2024-01-29 02:36:40 +0530

    Add files via upload

---
+++ login.js
@@ -0,0 +1,12 @@
+btn.onclick = function() {
+    const btn = document.getElementById("btn");
+    let name = document.getElementById("name");
+    let email = document.getElementById("email");
+    let password = document.getElementById("password");
+
+
+    alert("Success!");
+
+}
+
+
---
+++ signup.js
@@ -0,0 +1,9 @@
+btn.onclick = function() {
+    const btn = document.getElementById("btn");
+    let email = document.getElementById("email");
+    let password = document.getElementById("password");
+
+
+    alert("Success!");
+
+}
